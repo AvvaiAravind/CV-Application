471736accd042d0eff7a97d585b0cbdc081262f4
Avvai Aravind <avvaiaravind@gmail.com>
2024-11-08 17:28:44 +0530

    added resume section and input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,153 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import {
+  personal as initialPersonal,
+  professional as initialProfessional,
+  educational as initialEducational,
+} from "./sampleData";
+import { IoIosArrowDown } from "react-icons/io";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  return <Body />;
+}
+
+function Body() {
+  const [personal, setPersonal] = useState(initialPersonal);
+  const [professional, setProfessioanal] = useState(initialProfessional);
+  const [educational, setEducational] = useState(initialEducational);
+
+  function handleChange(e) {
+    // e.preventDefault();
+
+    const id = e.target.id;
+    let input = e.target.value;
+    if (personal.some((item) => item.id === id)) {
+      setPersonal((personal) =>
+        personal.map((item) =>
+          item.id === id ? { ...item, value: input } : item
+        )
+      );
+    } else if (professional.some((item) => item.id === id)) {
+      setProfessioanal((professional) =>
+        professional.map((item) =>
+          item.id === id ? { ...item, value: input } : item
+        )
+      );
+    } else if (educational.some((item) => item.id === id)) {
+      setEducational((educational) =>
+        educational.map((item) =>
+          item.id === id ? { ...item, value: input } : item
+        )
+      );
+    }
+  }
+  function handleSave(e) {
+    const parent = e.target..
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="input-section">
+        <UserInputSection
+          title={"Personal Details"}
+          section={personal}
+          onChange={handleChange}
+          onClick={handleSave}
+        />
+        <UserInputSection
+          title={"Educational Qualification"}
+          section={educational}
+          onChange={handleChange}
+          onClick={handleSave}
+        />
+        <UserInputSection
+          title={"Professional Experience"}
+          section={professional}
+          onChange={handleChange}
+          onClick={handleSave}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="resume-section">
+        <Resume
+          personal={personal}
+          professional={professional}
+          educational={educational}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function UserInputSection({ title, section, onChange, onClick }) {
+  return (
+    <section className="user-input-sect">
+      <div className="header-section">
+        <h2>{title}</h2>
+        <button type="button">
+          <IoIosArrowDown />
+        </button>
+      </div>
+      <form>
+        {section.map((item) => {
+          return (
+            <div key={item.id} className="input-field">
+              <label htmlFor={item.id}>{item.label}</label>
+              <input type="text" id={item.id} onChange={onChange} />
+            </div>
+          );
+        })}
+        <div className="button-container">
+          <button type="button" onClick={onClick}>
+            Save
+          </button>
+          <button type="button" onClick={onClick}>
+            Reset
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+function Resume({ personal, professional, educational }) {
+  return (
+    <>
+      <div className="personal-section">
+        <PersonalSection personal={personal} />
+      </div>
+      <div className="educational-section">
+        <RemainingSection content={educational} title="Education" />
+      </div>
+      <div className="professional-section">
+        <RemainingSection content={professional} title="Experience" />
+      </div>
+    </>
+  );
+}
+
+function PersonalSection({ personal }) {
+  return <>{personal.map((item) => renderPersonal(item))}</>;
+}
+
+function renderPersonal(item) {
+  switch (item.label) {
+    case "Full Name":
+      return <h1 key={item.label}>{item.value}</h1>;
+    default:
+      return <p key={item.label}>{item.value}</p>;
+  }
+}
+
+function RemainingSection({ content, title }) {
+  return (
+    <>
+      <h2>{title}</h2>
+      {content.map((item) => (
+        <p key={item.id} className={item.id}>
+          {item.value}
+        </p>
+      ))}
+    </>
+  );
+}
